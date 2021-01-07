7c4926b49e6a5a6c656f664e34f6bc2b1f08a586
KAgaev <kenkosh@yandex.ru>
2021-01-07 20:10:29 +0300

    завершает верстку

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,6 @@
 import { ieFix } from './utils/ie-fix';
 import { iosVhFix } from './utils/ios-vh-fix';
 
-import { initModals } from './modules/init-modals';
-import { menuToggler, onMenuTogglerToggleClass } from './modules/toggler';
-
 import { initSkillbox, skillboxList } from './modules/skillbox';
 
 // Utils
@@ -14,11 +11,6 @@ iosVhFix();
 
 // Modules
 // ---------------------------------
-
-initModals();
-if (menuToggler) {
-  menuToggler.addEventListener('click', onMenuTogglerToggleClass);
-}
 
 if (skillboxList) {
   skillboxList.forEach((item) => initSkillbox(item));
